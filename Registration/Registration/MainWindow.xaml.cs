e760978fdb0b7fbeb5c70501c683872d30765d7b
darya-genious <dasha2002573@gmail.com>
2024-07-17 19:43:16 +0300

    '17/07'

---
+++ Registration/Registration/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace Registration
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string log_in;
+        private string pass_word;
+        static string path = "C:\\Users\\AseR\\Desktop\\files\\Registration\\users.txt";
+        private string[] authoPair = new string[2];
+        private bool show = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            log_in=login.Text;
+        }
+
+        private void Registration()
+        {
+            pass_word = password.Password;
+            if (!CheckPassword(pass_word))
+                MessageBox.Show("Пароль должен содержать минимум 1 спец символ, заглавную букву и цифру");
+            else
+            {
+                if (!CheckUserExists(log_in))
+                    MessageBox.Show("Пользователь с таким именем уже существует");
+                else
+                {
+                    using (StreamWriter sw = new StreamWriter(path, true)) //для автоматического закрытия потоков
+                    {
+                        sw.WriteLine($"{log_in}-{pass_word}");
+                    }
+
+                    MessageBox.Show("Вы успешно зарегистрированы!");
+                }
+            }
+           
+        }
+
+        private bool CheckUserExists(string username)
+        {
+            bool checkExisting = false;
+            string line;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] loginPassword = line.Split('-');
+                    if (username == loginPassword[0])
+                    {
+                        authoPair=loginPassword;
+                        checkExisting = true;
+                        break;
+                    }
+                }
+
+                if (checkExisting)
+                    return false;
+                return true;
+            }
+
+        }
+
+        private void registr_Click(object sender, RoutedEventArgs e)
+        {
+            Registration();
+        }
+        
+        private bool CheckPassword(string new_password)
+        {
+            string pattern = @"[!@#$%^&*(),.?:{}\|<>]";
+
+            if (new_password.Any(char.IsUpper) && Regex.IsMatch(new_password, pattern) && new_password.Any(char.IsDigit))
+                return true;
+            else return false;
+        }
+
+        private void Authorization()
+        {
+            pass_word = password.Password;
+
+            if (!CheckUserExists(log_in))
+            {
+                if (authoPair[1] == pass_word)
+                    MessageBox.Show("Вы успешно вошли в систему");
+                else MessageBox.Show("Пароль неверный");
+            }
+            else MessageBox.Show("Пользователя с таким логином не сущестсвует");
+        }
+        
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+            Authorization();
+         
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!show)
+            {
+                show_password.Source =
+                    new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\Registration\\close.png"));
+                txtBlock.Text = password.Password;
+                txtBlock.Visibility=Visibility.Visible;
+                password.Visibility =Visibility.Hidden;
+                show = true;
+            }
+            else
+            {
+                show_password.Source =
+                    new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\Registration\\open.png"));
+                password.PasswordChar = '●';
+                txtBlock.Visibility = Visibility.Hidden;
+                password.Visibility = Visibility.Visible;
+                show = false;
+            }
+        }
+    }
+}
---
+++ cats-library/WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+         
+            if (sender == cats1.Items[0])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\kerl.jpg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[1])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\mein-kun1.jpg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[2])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\pers.jpeg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[3])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\regdoll.jpeg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[4])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\britan.jpg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[5])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\nevskaya.jpeg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[6])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\turkish.jpg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[7])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\norv.jpg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[8])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\siberian.jpeg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+            if (sender == cats1.Items[9])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\laperm.jpeg", UriKind.Absolute));
+                img1.Source = new_source;
+                img1.InvalidateVisual();
+            }
+        }
+
+        private void ListBoxItem_Selected2(object sender, RoutedEventArgs e)
+        {
+            if (sender == cats2.Items[0])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\karaket.jpeg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[1])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\karakal.jpeg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[2])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\kamysh.jpg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[3])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\bob.jpg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[4])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\anatol.png", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[5])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\burman.jpeg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[6])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\bombey.jpeg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[7])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\bengal.jpeg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[8])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\american.jpg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+            if (sender == cats2.Items[9])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\korat.jpeg", UriKind.Absolute));
+                img2.Source = new_source;
+                img2.InvalidateVisual();
+            }
+        }
+
+        private void ListBoxItem_Selected3(object sender, RoutedEventArgs e)
+        {
+
+            if (sender == cats3.Items[0])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\peterbold.jpg", UriKind.Absolute));
+                img3.Source = new_source;
+                img3.InvalidateVisual();
+            }
+            if (sender == cats3.Items[1])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\donskoy.jpeg", UriKind.Absolute));
+                img3.Source = new_source;
+                img3.InvalidateVisual();
+            }
+            if (sender == cats3.Items[2])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\kanadsky.jpg", UriKind.Absolute));
+                img3.Source = new_source;
+                img3.InvalidateVisual();
+            }
+            if (sender == cats3.Items[3])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\ykr.jpeg", UriKind.Absolute));
+                img3.Source = new_source;
+                img3.InvalidateVisual();
+            }
+            if (sender == cats3.Items[4])
+            {
+                BitmapImage new_source = new BitmapImage(new Uri("C:\\Users\\AseR\\Desktop\\files\\cats\\elf.jpeg", UriKind.Absolute));
+                img3.Source = new_source;
+                img3.InvalidateVisual();
+            }
+        }
+    }
+}
